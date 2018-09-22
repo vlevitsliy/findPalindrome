4f82c7d2cb4cf8c93040b86e8c775e9026a11664
vova <vovanlev61@gmail.com>
2018-09-22 21:17:20 +0300

    Решение с bigInteger

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,39 @@
+import java.math.BigInteger;
+
 public class Main {
     public static void main (String argsp[]) {
-        calc(760);
+        calc(new BigInteger("196"));
+//        System.out.println(a.add(b));
     }
 
-    public static long calc (long number){
-        long revert=revertNumber(number);
-        long result=number+revert;
-        System.out.println(revert+"+"+number+"="+result);
+    public static BigInteger calc (BigInteger number){
+        BigInteger revert=revertNumber(number);
+        BigInteger result=number.add(revert);
+        System.out.println(revert.toString()+"+"+number.toString()+"="+result.toString());
         if (!isPalindrome(result)) {
             calc(result);
         }
-        return 0;
+        return new BigInteger("0");
     }
 
-    public static long revertNumber (long number){
-        long palindrome = number;
-        long reverse = 0;
+    public static BigInteger revertNumber (BigInteger number){
+        BigInteger palindrome = new BigInteger(number.toString());
+        BigInteger reverse = new BigInteger("0");
+        BigInteger ten = new BigInteger("10");
 
-        while (palindrome != 0) {
-            long remainder = palindrome % 10;
-            reverse = reverse * 10 + remainder;
-            palindrome = palindrome / 10;
+        while (palindrome.compareTo(BigInteger.ZERO) != 0) {
+            BigInteger remainder = new BigInteger(palindrome.toString());
+            remainder = remainder.remainder(ten);
+            reverse = reverse.multiply(ten).add(remainder);
+            palindrome = palindrome.divide(ten);
         }
 
         return reverse;
     }
 
-    public static boolean isPalindrome (long number) {
-        long revert = revertNumber(number);
-        if (number==revert) {
+    public static boolean isPalindrome (BigInteger number) {
+        BigInteger revert = revertNumber(number);
+        if (number.equals(revert)) {
             return true;
         }
 
